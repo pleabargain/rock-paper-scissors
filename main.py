f75c97a186880d05d516efd32d62c79471372e6d
Dennis <dennisgdaniels@gmail.com>
2022-10-21 08:46:37 +0400

    updated

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ class Weapon(str, Enum):
     rock = 'rock'
     paper = 'paper'
     scissors = 'scissors'
+    spock = 'spock'
+    lizard = 'lizard'
 
 @app.get('/')
 async def read_root():
@@ -17,17 +19,34 @@ async def read_root():
 async def shoot(weapon: Weapon):
 
     game_key = {
-        ('rock', 'rock'): "It's a tie.",
-        ('rock', 'paper'): "You lost.",
+        ('lizard', 'spock'): 'You lose! lizard poisons spock',
+        ('lizard', 'paper'): 'You win! lizard eats paper',
+        ('lizard', 'rock'): 'You lose! rock crushes lizard',
+        ('lizard', 'scissors'): 'You lose! scissors decapitates lizard',
+        ('lizard', 'lizard'): 'Draw! lizard and lizard',
+    
+        ('spock', 'spock'): 'Draw! spock and spock',
+        ('spock', 'paper'): 'You lose! paper disproves Spock',
+        ('spock', 'rock'): 'You win! Spock vaporizes rock',
+        ('spock', 'scissors'): 'You win! Spock smashes scissors',
+        ('spock', 'lizard'): 'You lose! Lizard poisons Spock',
+        
+        ('rock', 'paper'): "You lose.",
         ('rock', 'scissors'): "You won!",
+        ('rock', 'lizard'): "Rock smashes lizard! You lost!!",
+        ('rock', 'rock'): "It's a tie.",
+
         ('paper', 'rock'): "You won!",
-        ('paper', 'paper'): "It's a tie.",
+        ('paper', 'spock'): "Paper disproves Spock! You won!",
         ('paper', 'scissors'): "You lost.",
-        ('scissors', 'rock'): "You lost.",
-        ('scissors', 'paper'): "You won!",
+        ('paper', 'paper'): "It's a tie.",
+
+        ('scissors', 'spock'): "Spock smashes scissors! You lost!",
+        ('scissors', 'rock'): "Rock smashes scissors!You lost.",
+        ('scissors', 'paper'): "Scissors cuts paper! You won!",
         ('scissors', 'scissors'): "It's a tie.",
     }
-    weapons = ['rock', 'paper', 'scissors']
+    weapons = ['rock', 'paper', 'scissors','spock','lizard']
     opp_weapon = weapons[randrange(0, 3)]
     message = game_key[(weapon, opp_weapon)]
     result = {'user_weapon': weapon, 'opponent_weapon': opp_weapon, 'message': message}
